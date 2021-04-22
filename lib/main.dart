eb3856107bfd260af4ad581853de97092ac991a4
Sebastián Salazar <sebanicosp@gmail.com>
2021-04-21 23:59:13 -0400

    add multBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tic_tac_toe/bloc_observer.dart';
 import 'package:tic_tac_toe/src/app.dart';
+import 'package:tic_tac_toe/src/blocs/game/game_bloc.dart';
 
 void main() {
   Bloc.observer = MyBlocObserver();
-  runApp(MyApp());
+  runApp(MultiBlocProvider(
+      providers: [BlocProvider(create: (BuildContext context) => GameBloc())],
+      child: MyApp()));
 }
