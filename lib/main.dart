d70fe624f97809481eb23e6a3652f78f8fec32c6
Sebastián Salazar <sebanicosp@gmail.com>
2021-04-21 21:55:45 -0400

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/bloc_observer.dart';
 import 'package:tic_tac_toe/src/app.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(MyApp());
 }
